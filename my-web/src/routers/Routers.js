cee606c3d6b7030bd8e98c4ef496b4b7b9482e99
Trần Nhật Linh <linh54977@donga.edu.vn>
2023-08-08 23:06:34 +0700

    Update file Routers about loading effect

--- my-web/src/routers/Routers.js
+++ my-web/src/routers/Routers.js
@@ -8,11 +8,13 @@ import Register from "../pages/register/Register";
 import ProductDetails from "../pages/productDetails/ProductDetails";
 import ProtectRoute from "./ProtectRoute";
 import ThankYou from "../pages/thank-you/ThankYou";
+import "spin.js/spin.css";
+import { Spinner } from "spin.js";
 const LazyShop = React.lazy(() => import("../pages/shop/Shop"));
-
 function Routers(props) {
   const navigate = useNavigate();
 
+  const customSpinner = new Spinner({ color: "rgb(0, 200, 133)" });
   // Sử dụng useEffect để thực hiện chuyển hướng đến trang home khi trang được tải lần đầu
   useEffect(() => {
     navigate("/home");
@@ -22,10 +24,20 @@ function Routers(props) {
     <Routes>
       <Route path="home" element={<Home />} />
       <Route path="login" element={<Login />} />
-      <Route
+      {/* <Route
         path="shop"
         element={
           <React.Suspense fallback="Loading...">
+            <LazyShop />
+          </React.Suspense>
+        }
+      /> */}
+      <Route
+        path="shop"
+        element={
+          <React.Suspense
+            fallback={<div ref={(el) => el && customSpinner.spin(el)} />}
+          >
             <LazyShop />
           </React.Suspense>
         }
